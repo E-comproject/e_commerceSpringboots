f0d2073348b1f2eafac090ca4d61003a4fd9b8c8
Alovenus <puchit2576@gmail.com>
2025-09-10 21:45:55 +0700

    kuyyyyy

--- backend/src/main/java/com/ecommerce/EcommerceApplication/controller/ProductController.java
+++ backend/src/main/java/com/ecommerce/EcommerceApplication/controller/ProductController.java
@@ -17,7 +17,7 @@ public class ProductController {
 
     @GetMapping
     public String getAllProducts() {
-        return "รายการสินค้าทั้งหมด";
+        return "รายการสินค้าทั้งหมด555555555";
     }
 
     @GetMapping("/{id}")
